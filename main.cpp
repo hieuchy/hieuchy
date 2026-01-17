429882e40ae83795ccd7fc60dfac19b7acf97509
hieuchy <hieuuu1223n@gmail.com>
2026-01-17 23:59:54 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,popcnt,lzcnt")
+
+#include <bits/stdc++.h>
+using namespace std;
+#define TASK ""
+#define pb push_back
+#define F first
+#define S second
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<long long> vl;
+typedef map<int,int> mp;
+typedef unsigned long long ull;
+typedef double db;
+typedef unordered_map<int,int> ump;
+typedef unordered_map<char,int> cump;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    if (fopen(TASK ".INP", "r")) {
+        freopen(TASK ".INP", "r", stdin);
+        freopen(TASK ".OUT", "w", stdout);
+    }
+
+
+    return 0;
+}
